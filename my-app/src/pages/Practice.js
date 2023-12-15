45e61e63aee9bff5db1d75d8b0195667dd7f0590
Vaibhavgupta99 <Gvaibhav224@gmail.com>
2023-12-15 23:17:40 +0530

    by Vaibhav

--- my-app/src/pages/Practice.js
+++ my-app/src/pages/Practice.js
@@ -1,12 +1,47 @@
-import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Button, TextInput, TouchableOpacity, Pressable, Alert } from 'react-native'
 import React, { useState } from 'react'
-/* import {AsyncStorage} from 'react-native'; */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+   const StoreData = async (value) => {
+    try {
+      await AsyncStorage.setItem('data',JSON.stringify(value));
+      alert('Data Stored Successfully')
+    } catch (e) {
+      // Error saving data
+      console.log(e)
+    }
+    // console.log(value)
+    // alert(JSON.stringify(value))
+  };
+
+
+   const GetData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('data');
+      /* console.log(value) */
+      
+      if (value != null) {
+        // We have data!!
+        // console.log(value);
+        const obj = JSON.parse(value)
+        alert(obj.name + ',' + obj.phone)
+        AsyncStorage.clear()
+      }
+      else{
+        console.log('No data is stored')
+      }
+    } catch (e) {
+      // Error retrieving data
+    }
+  };
 
 
 export default function Practice({navigation}) {
 
     const [value, setValue] = useState({})
-    console.log(value.name)
+    /* console.log(value.name) */
+
+    
 
   return (
     <View style={styles.container}>
@@ -14,7 +49,12 @@ export default function Practice({navigation}) {
         <Text>{value.phone}</Text>
         <TextInput onChangeText={(e)=> {setValue({...value, name:e})}} placeholder='Name' style={styles.input}></TextInput>
         <TextInput onChangeText={(e)=> {setValue({...value, phone:e})}} placeholder='Phone' style={[styles.input,{marginBottom:30,}]}></TextInput>
-        <TouchableOpacity style={{width:'50%'}} > 
+
+        <Pressable style={styles.btn}><Button title='Store Value' onPress={()=> {StoreData(value)}} /></Pressable>
+
+        <Pressable style={styles.btn}><Button title='Get Value' onPress={()=> {GetData()}} /></Pressable>
+
+        <TouchableOpacity style={{width:'50%', margin:10,}} > 
         <Button title='Home'
             onPress={()=> navigation.navigate('Home')}
         />
@@ -38,5 +78,8 @@ input: {
     width: '80%',
     marginBottom:10,
     fontSize:18,
+}, 
+btn: {
+    margin: 10,
 }
 })
